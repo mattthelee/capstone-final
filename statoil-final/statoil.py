ee95755a3690e9fd61a149c3456093439f549ce4
unknown <matt_lee92@hotmail.co.uk>
2018-02-03 17:09:51 +0000

    clearing up python

--- statoil-final/statoil.py
+++ statoil-final/statoil.py
@@ -6,10 +6,6 @@ Created on Sat Nov  4 17:05:40 2017
 @author: leem
 """
 
-# need to find a sensible way to import the data. 
-# Would like to handle the data in something like a datframe 
-# am i able to load all the data into memory
-
 import json
 import pandas as pd
 import keras
@@ -60,17 +56,6 @@ def z_score(image_list):
 def plotImage(standardised_list):
     standardised_image = np.array(standardised_list).reshape(75,75)
     plt.imshow(standardised_image)
-    return
-    
-def showArrayImage(array):
-    flat_array = array.flatten()
-    new_array = np.asarray(map(lambda v: int(v - min(flat_array)), flat_array))
-    new_array = np.asarray(map(lambda v: 255*v/new_array.max(), new_array))
-
-    new_array.shape = (75,75)
-    arrayImage = Image.fromarray(new_array, mode='L')
-    arrayImage.show()
-    arrayImage.save('my_not_iceberg_band_2.png')
     return
 
 def showListImage(theList):
@@ -126,35 +111,8 @@ avg_angle = np.mean(filter(lambda x: x != 'na' ,training_frame['inc_angle']))
 # Replace the na's with the average angle
 training_frame['inc_angle'] = training_frame['inc_angle'].replace('na',avg_angle)
 
-# have to convert y_train to a numpy array as dataframe has a keras bug
-#y_train = np.asarray(pd.get_dummies(training_frame['is_iceberg']))
-
 # Converting angle to a sin as the 
 angle_factor = [ np.sin(angle*np.pi/180.0) for angle in training_frame['inc_angle']]
-
-#band_1_arrays = np.asarray(map(lambda v : convertBand(v), training_frame['band_1']))
-#band_2_arrays = np.asarray(map(lambda v : convertBand(v), training_frame['band_2']))
-#channelled_bands = convertToTensor(training_frame,angle_factor)
-
-# TODO experiment without horizontal, vertical and rotations
-# Sun direction might affect the light see so might not want to lose that information
-
-# Als0 need to check that having it as a channel isn't just something done for different colours as the polarization is a bit different
-
-x_train = frameToImagesTensor(training_frame)
-#x_test = frameToImagesTensor(testing_frame)
-'''
-gen = ImageDataGenerator(horizontal_flip = True,
-                         vertical_flip = True,
-                         zoom_range = 0.2,
-                         rotation_range = 360)
-
-training_flow = gen.flow(x_train)
-testing_flow = gen.flow(x_test)
-
-my_model = createModel()
-my_model.fit_generator(training_flow,steps_per_epoch=24,epochs= 150)
-'''
 
 mysgd = SGD(0.01,0.5,0.0001,True)
 myrmsprop = ""
@@ -205,7 +163,6 @@ for i in range(k):
     train_log_loss = log_loss(cv_y_training_samples,np.asarray(train_pred),0.0000001)
     total_train_log_loss += train_log_loss
     total_test_log_loss += test_log_loss
-    print "Avg log loss: Test %03d,  Train:%03d" % (train_log_loss,test_log_loss)
 
 avg_test_log_loss = total_test_log_loss/float(k)
 avg_train_log_loss = total_train_log_loss/float(k)
@@ -214,60 +171,3 @@ print "Avg log loss: Test"
 print  avg_test_log_loss
 print "Train:"
 print avg_train_log_loss
-
-#immediate steps
-# try different optimiser, e.g. adam optimiser
-# try tensorflow gpu instead (may need to buy ram for personal laptop)
-    
-
-#immediate steps for kernal based stuff
-# Need to remind myself what form the data is in when i pass it to the model
-# need to convert daata into a flow and the pass into image generator
-# need to pass data into the model
-
-# Next steps:
-# icebergs and ships are only a few pixels wide
-# L- should therefore have the image cropped to only an area that contains the object
-# L- this will reduce redundent data and speed up the algorithm see: http://elib.dlr.de/99079/2/2016_BENTES_Frost_Velotto_Tings_EUSAR_FP.pdf
-# should do a 3d plot of the array too as shown in above paper
-# Add in image gerneator and use it to create additional images with distortions
-# use vg16 pretrained nn to help, also try the other ones available from image net
-# Setup an aws instance to allow the models to be long running
-# consider writing in pyspark compatible format
-# Add additional conv layers
-# L- research why additional conv layers would improve accuracy rather than improve performance
-# could consider looking at ship wake as form of detection
-# the higher the wind, the more bragg scattering, the more cluttered the ocean image will be
-# the higher winds also make for improved wake detection
-# clutter decreases with incidence angle
-
-#sentinel 1 paper, can i use the following data? : At DRDC Ottawa, shore-based commercial AIS data were
-#obtained in conjunction with several RADARSAT-1 and Envisat ASAR trials [32] [34] [36], for
-#compilation of a database of more than 4000 validated ship signatures that may be used to
-#improve models of ship RCS and its variability.
-
-#Should consider chaning the dB into 0,255 similar to in Feature extraction of dual-pol SAR imagery for sea ice image segmentation I
-# I need to find the range of the decibels,set one to 0, set the highest to 255 and apply some method to split the data across the range
-# Need to check the precision of the  decibels and therefore whether this process results in loss of sig fig
-    #l- from the same paper should consider perfroming the max gradient preprocessing, it's simple and was effective for them
-
-# Should add to reading list: 
-
-
-# Howell, C., Mills, J., Power, D., Youden, J., Dodge, K., Randell, C., Churchill, S., and Flett,
-#D. (2006). A multivariate approach to iceberg and ship classification in HH/HV ASAR
-#data. Proc. 2006 International Geoscience and Remote Sensing Symposium (IGARSS
-#2006). CD-ROM proceedings. 31 July to 4 Aug. 2006, Denver, USA.
-
-
-#Henschel, M.D., and Livingstone, C.E. (2006). Observation of vessel heave with airborne
-#SAR. Proc. OceanSAR 2006 – The Third Workshop on Coastal and Marine Applications
-#of SAR, St. John’s, NL, Canada, 23 to 25 October 2006.
-
-#Power, D., Youden, J., Lane, K., Randell, C., and Flett, D. (2001). Iceberg detection
-#capabilities of RADARSAT synthetic aperture radar. Canadian Journal of Remote Sensing,
-#27(5), 476-486.
-
-#Pond, S., and Pickard, G.L. (1983). Introductory Dynamical Oceanography, 2 nd Edition.
-#Pergamon Press, Toronto.
-
